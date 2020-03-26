8495011ffba95b2178d7e06816ed0168725d146b
Yu Yan <miracleyanyu@gmail.com>
2020-03-26 16:59:11 -0700

    Add Question Server

---
+++ services/question.service.server.js
@@ -0,0 +1,8 @@
+let questions = require('./questions');
+
+findQuestionsForQuiz = (quizId) =>
+    questions.filter(question => question.quizId === quizId);
+
+module.exports = {
+  findQuestionsForQuiz
+};
