1f52c6f1f0ab728dcce796078ba48d63b2d53e57
Yu Yan <miracleyanyu@gmail.com>
2020-04-05 20:01:51 -0700

    Update Heroku Settings

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -20,5 +20,9 @@ require('./controllers/question.controller.server')(app);
 require('./controllers/university.controller.server')(app);
 require('./controllers/student.controller.server')(app);
 require('./controllers/answer.controller.server')(app);
+
+app.get('/', (req, res) => {
+  res.send('Assignment 9 NodeJs Server.')
+});
 
 app.listen(3000);
